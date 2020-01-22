5aa4d82922e8c50dd903b67ab0ac06ce0ad6709d
Timur Garipov <tbgaripov@gmail.com>
2020-01-22 19:31:31 +0300

    fff

--- blog/hello-world.md
+++
@@ -1,6 +0,0 @@
----
-path: /firstpage
-date: 2020-01-16T22:41:05.914Z
-title: hello world
----
-hi
---
+++ src/templates/project.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import "../styles/project.scss";
+
+class ProjectTemplate extends Component {
+    render () {
+        return (
+            <section className="project">
+                <img className="project__render"
+                    src={this.props.project.renderUrl}
+                    src= {this.props.project.renderAlt}
+                />
+                <div className="project__description">
+                    <h2 className="project__title">{this.props.project.title}</h2>
+                    <span className="project__year">{this.props.project.year}</span>
+                    <p className="project__p">{this.props.project.description}</p>
+                </div>
+                <img className="project__scheme"
+                    src={this.props.project.schemeUrl}
+                    src= {this.props.project.schemeAlt}
+                />
+            </section>
+        )
+    }
+}
+
+ProjectTemplate.PropTypes = {
+    project: PropTypes.shape({
+        title: PropTypes.string,
+        year: PropTypes.string,
+    }),
+};
+
+export default ProjectTemplate;
