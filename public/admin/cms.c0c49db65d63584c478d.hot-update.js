afd03fdf4b0954c89c04e1022466739ce5e933a8
Timur Garipov <tbgaripov@gmail.com>
2020-02-04 00:04:27 +0300

    changed color and size of template page text

---
+++ public/admin/cms.c0c49db65d63584c478d.hot-update.js
@@ -0,0 +1,22 @@
+webpackHotUpdate("cms",{
+
+/***/ "./src/styles/temporary-index.scss":
+/*!*****************************************!*\
+  !*** ./src/styles/temporary-index.scss ***!
+  \*****************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+    if(true) {
+      // 1580763807530
+      var cssReload = __webpack_require__(/*! ./node_modules/mini-css-extract-plugin/dist/hmr/hotModuleReplacement.js */ "./node_modules/mini-css-extract-plugin/dist/hmr/hotModuleReplacement.js")(module.i, {"hmr":true,"locals":false});
+      module.hot.dispose(cssReload);
+      module.hot.accept(undefined, cssReload);
+    }
+  
+
+/***/ })
+
+})
+//# sourceMappingURL=cms.c0c49db65d63584c478d.hot-update.js.map
